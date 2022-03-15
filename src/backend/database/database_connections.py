42cbc468228a4f231de52fcba09ba151e73afe48
Lucas Silva <l.silva@picalike.com>
2022-03-15 20:51:43 -0300

    More fixings and compose new way

--- src/backend/database/database_connections.py
+++ src/backend/database/database_connections.py
@@ -2,10 +2,9 @@ import asyncpg
 
 async def create_postgres_pool():
     connection_settings  = {
-        "user":"enem",
-        "password":"catapimbas",
-        "host":"192.168.0.108",
+        "user":"postgres",
+        "password":"asdfasdf",
         "port":5432,
     }
-    print(connection_settings)
+
     return await asyncpg.create_pool(**connection_settings)
